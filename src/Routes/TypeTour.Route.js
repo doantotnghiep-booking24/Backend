45e1021137009562e96e07d16c1b72aa2f800454
TranQuocDuong204 <tranquocduong2004vta@gmail.com>
2024-11-23 20:42:09 +0700

    fixcors

--- src/Routes/TypeTour.Route.js
+++ src/Routes/TypeTour.Route.js
@@ -2,7 +2,7 @@ import express from 'express'
 import TypeTour_Controller from '../App/Controllers/TypeTour_Controller.js'
 import AuthUser from '../App/MiddleWare/Decentralization/AuthUser.js'
 const Router = express.Router()
-Router.get('/GetAllTypeTour',AuthUser(["Admin", "User"]), TypeTour_Controller.GetAllTypeTour) // http://localhost:3001/V2/TypeTour/GetAllTypeTour
+Router.get('/GetAllTypeTour', TypeTour_Controller.GetAllTypeTour) // http://localhost:3001/V2/TypeTour/GetAllTypeTour
 Router.post('/CreateTypeTour',AuthUser(["Admin"]), TypeTour_Controller.CreateTypeTour) // http://localhost:3001/V2/TypeTour/CreateTypeTour
 Router.post('/UpdateTypeTour/:id',AuthUser(["Admin"]), TypeTour_Controller.UpdateTypeTour) // http://localhost:3001/V2/TypeTour/UpdateTypeTour/66a46a2852a8d1dbe7a70291
 Router.post('/DeleteTypeTour/:id',AuthUser(["Admin"]), TypeTour_Controller.DeleteTypeTour) // http://localhost:3001/V2/TypeTour/DeleteTypeTour/66a46a2852a8d1dbe7a70291
