f463f7f61be47172128c50b738e31b2f68e1bfc3
TranQuocDuong204 <tranquocduong2004vta@gmail.com>
2024-12-14 14:59:03 +0700

    up

--- src/Routes/Categories.Route.js
+++ src/Routes/Categories.Route.js
@@ -6,5 +6,5 @@ Router.get('/GetCategories', Categories_Controller.GetAllCategory) // http://loc
 Router.post('/CreateCategory',AuthUser([ "Admin", "Staff"]), Categories_Controller.CreateCategory) // http://localhost:3001/V2/Category/CreateCategory
 Router.post('/UpdateCategory/:id', AuthUser([ "Admin", "Staff"]), Categories_Controller.UpdateCategory) // http://localhost:3001/V2/Category/UpdateCategory/66a46a2852a8d1dbe7a70291
 Router.post('/DeleteCategory/:id',AuthUser([ "Admin", "Staff"]), Categories_Controller.DeleteCategory) // http://localhost:3001/V2/Category/DeleteCategory/66a46a2852a8d1dbe7a70291
-Router.get('/', Categories_Controller.GetAllCategory)
+Router.get('/', Categories_Controller.GetAllCategory);
 export default Router
