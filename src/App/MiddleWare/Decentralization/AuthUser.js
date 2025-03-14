260591db09e367e9c8cd5203ef5fb24813b93430
nHung-k419 <144356485+nHung-k419@users.noreply.github.com>
2025-03-14 22:06:31 +0700

    Update AuthUser.js

--- src/App/MiddleWare/Decentralization/AuthUser.js
+++ src/App/MiddleWare/Decentralization/AuthUser.js
@@ -4,9 +4,9 @@ import Connection from '../../../Config/db/index.js';
 const AuthUser = (permissions) => {
     return async (req, res, next) => {
         try {
-            const originPort = req.headers.origin.split(':').pop();
+            const originPort = req.headers
             console.log('originPort',originPort)
-            const cookieName = originPort === '5173' ? 'auth' : 'authAdmin';
+            const cookieName = originPort === 'https://frontend-booking-ovf1.onrender.com' ? 'auth' : 'authAdmin';
             const user = req.cookies[cookieName];
 console.log(user);
 
